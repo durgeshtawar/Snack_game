6d3574b0d804ada8a36d0525873eb5a8f7ae7ae4
Durgesh Tawar <adityatawar58@gmail.com>
2024-01-05 12:09:46 +0530

    snack game using react js

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Board from './Board/Board'
+import './App.css'
+
+function App() {
+  
+  return (
+    <div className='app'>
+      <Board />
+      
+    </div>
+  )
+}
+
+export default App
